e43e8d5b103d19c228c1fd8ea3f4bd38352524b0
8Evgenii8 <jiganpro77@gmail.com>
2024-10-16 22:31:00 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Евгений'
+print(name, type(name))
+age = 38
+print(age, type(age))
+new_age = + 1
+print(age + new_age , type(age + new_age))
+is_student = True
+print(is_student, type(is_student))
